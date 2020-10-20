8742511b71ecb0e24447e12602229c775c090b61
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-10-20 00:25:24 +0000

    return the string and the square of an int/float

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated to_kv function"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return a tuple with string and the square of an int and/or float"""
+    return (str, v**2)
