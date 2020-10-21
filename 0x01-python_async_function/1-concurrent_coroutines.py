e4862a5e01ca092eddf2af06fd2c6a13fbf5e242
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-10-21 00:20:10 +0000

    execute mutiple coros at the same time with async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Execute multiple coroutines at the same time with async"""
+from asyncio import as_completed
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays in ascending order"""
+    y = [wait_random(max_delay) for _ in range(n)]
+    return [await task for task in as_completed(y)]
