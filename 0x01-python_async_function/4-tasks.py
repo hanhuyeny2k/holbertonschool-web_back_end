1843f95d430280c1ace25280c0942184a160cd59
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-10-21 01:11:40 +0000

    task 4 in relation to task 3

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Call task_wait_random function in wait_n function"""
+"""Call task_wait_n function in task_wait_random function"""
 from asyncio import as_completed
 from typing import List
 
@@ -9,4 +9,4 @@ twr = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays in ascending order"""
     y = [twr(max_delay) for _ in range(n)]
-    return [await task for task in as_completed(twr)]
+    return [await task for task in as_completed(y)]
