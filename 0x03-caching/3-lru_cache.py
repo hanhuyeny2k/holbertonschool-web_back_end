3438c6a22900091dacdaa1946e268bc5e350b7a7
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-10-27 02:56:56 +0000

    LRU cache

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Get and retrieve items using LRU caching """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU cache"""
+    def __init__(self):
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """add data to the cache"""
+        if key is None or item is None:
+            return
+        if key in self.keys:
+            self.keys.remove(key)
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            discard = self.keys.pop(0)
+            print("DISCARD: {}".format(discard))
+            del self.cache_data[discard]
+        self.keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve data from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
