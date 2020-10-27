8abba6e8699bab75ea15e7a84b9fcbcfde0c3530
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-10-27 02:23:25 +0000

    fifo

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" FIFO caching """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ """
+    def __init__(self):
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        if key in self.keys:
+            self.keys.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.keys.pop(0)
+            print("DISCARD: {}".format(discard))
+            del self.cache_data[discard]
+        self.keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve data from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+
