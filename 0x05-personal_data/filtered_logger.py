b31950f39b047bbb7774c0ff250a3deffe36f056
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-02 23:55:57 +0000

    implement format method to filter values

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,6 +5,25 @@ Regex-ing
 from typing import List
 import re
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format a record"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -12,5 +31,5 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     substitute_msg = message
     for field in fields:
         substitute_msg = re.sub(
-            f'(?<={field}=)[^{separator}]*', redaction, substitute_msg)
+                f'(?<={field}=)[^{separator}]*', redaction, substitute_msg)
     return substitute_msg
