3ce6d901cbfd551f2cfcc8c66c53e7e76cdb320d
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-03 04:59:54 +0000

    check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
-"""Encrypting passwords"""
+"""Check valid password"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """Return a salted, hashed password"""
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate the provided password matches the hashed password"""
+    return bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password)
