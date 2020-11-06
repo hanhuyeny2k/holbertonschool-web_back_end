2f0afbe7f800b8be55d11d0eef0f88dd6cb44220
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-06 06:32:21 +0000

    error handler: forbidden

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,14 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized():
     """ GET /api/v1/unauthorized to return error message"""
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden to return error message"""
+    abort(403)
