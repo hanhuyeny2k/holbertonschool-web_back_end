0a2e485addd67e14310992b2c2b220fc7e3d1fa6
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-06 07:02:14 +0000

    routes don't need authentication

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -3,12 +3,22 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """manage API authentication"""
     
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """return False"""
-        return False
+        """define which routes don't need authentication"""
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
+        return True
     
     def authorization_header(self, request=None) -> str:
         """return None"""
