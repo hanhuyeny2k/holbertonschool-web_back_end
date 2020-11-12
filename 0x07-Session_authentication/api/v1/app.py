b9274b9d3a277c1e6497b4bae02145a4e5215e85
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-12 05:17:13 +0000

    instantiate auth with sessionexpauth

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -22,6 +22,9 @@ elif getenv('AUTH_TYPE') == 'auth':
 elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
 
 
 @app.before_request
