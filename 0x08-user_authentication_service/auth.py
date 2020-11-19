1c3a640befe2d580860ba2d2822b36d7b9e49bbc
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-19 03:46:20 +0000

    register user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 """Authorization Class"""
-from flask import request
-from typing import List, TypeVar
-from os import getenv
 from db import DB
 from user import User
-import uuid
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -16,31 +12,16 @@ def _hash_password(password: str) -> str:
 
 
 class Auth:
-    """manage API authentication"""
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """define which routes don't need authentication"""
-        if path is None or not excluded_paths:
-            return True
-        for i in excluded_paths:
-            if i.endswith('*') and path.startswith(i[:-1]):
-                return False
-            elif i in {path, path + '/'}:
-                return False
-        return True
+    """Auth class to interact with the authentication database"""
 
-    def authorization_header(self, request=None) -> str:
-        """request validation"""
-        if request is None:
-            return None
-        return request.headers.get('Authorization')
+    def __init__(self):
+        """initialization"""
+        self._db = DB()
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """return None"""
-        return None
-
-    def session_cookie(self, request=None):
-        """return a cookie value from a request"""
-        if request is None:
-            return None
-        else:
-            return request.cookies.get(getenv('SESSION_NAME'))
+    def register_user(self, email: str, password: str) -> User:
+        """register user using email and password"""
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
