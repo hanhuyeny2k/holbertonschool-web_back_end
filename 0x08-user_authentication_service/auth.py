bf587eda73b5d34ec03324df1cfb9654c15770ef
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-19 03:56:35 +0000

    credentials validation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -25,3 +25,12 @@ class Auth:
                 raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except Exception:
+            return False
