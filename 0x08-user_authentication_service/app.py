fe43748ceb30389875a05ee2b98b8b27d610dbb1
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-11-19 03:53:56 +0000

    register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,15 +3,29 @@
 SQLAlchemy model User
 """
 from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def hello():
     """ GET/Return welcome message"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'])
+def register():
+    """register user using email and password"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
